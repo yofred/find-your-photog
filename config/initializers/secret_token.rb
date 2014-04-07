6bded5d059bb0acc16217a486cb6c4b95dfdd645
Yofred Moik <ytmoik@gmail.com>
2014-04-07 16:11:19 -0400

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Findyourweddingphotographer::Application.config.secret_token = '205ed24b40c33eb65f4b69440acc863c4da288112559cdd95d366e1fb73b1f009bc46e416f999e084d6272cb675ae3e95129b42f8ac542625840ef89f2b9e39c'
