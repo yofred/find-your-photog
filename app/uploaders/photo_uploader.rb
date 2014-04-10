3df7708fb17f07533d801f0a9c11d2197610716b
Yofred Moik <ytmoik@gmail.com>
2014-04-10 12:29:47 -0400

    images are responsive

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -36,10 +36,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [280, 280]
   end
   
-  version :large_thumb do
-    process :resize_to_fill => [400, 400]
+  version :show do
+    process :resize_to_fill => [800, 800]
   end
-
+  
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
