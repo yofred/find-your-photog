f8084ada6748125609876f3cc780c6657bc9900c
Yofred Moik <ytmoik@gmail.com>
2014-04-10 12:09:53 -0400

    thumbnail generation added

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -32,9 +32,13 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fill => [280, 280]
+  end
+  
+  version :large_thumb do
+    process :resize_to_fill => [400, 400]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
