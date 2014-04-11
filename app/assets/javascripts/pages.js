4397745ec1a5c84560ce6e353627a40a5a1d64e8
Yofred Moik <ytmoik@gmail.com>
2014-04-11 12:34:15 -0400

    latest version

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,23 +1,25 @@
-$(document).ready(function(){  
-  $("#f00px_button").on('click', function(){
-    $("#findyourphotog").hide();
-    $("#f00px").show();
-  });
-  
-  $("#findyourphotog_button").on('click', function(){
-    $("#f00px").hide();
-    $("#findyourphotog").show();
-  }); 
-});
+// $(document).ready(function(){  
+//   $("#f00px_button").on('click', function(){
+//     $("#findyourphotog").hide();
+//     $("#f00px").show();
+//   });
+//   
+//   $("#findyourphotog_button").on('click', function(){
+//     $("#f00px").hide();
+//     $("#findyourphotog").show();
+//   }); 
+// });
 
 $(document).ready(function(){  
   $("#f00px_button").on('click', function(){
     $("#findyourphotog").hide();
-    $("#f00px").show();
+    $("#f00px").hide();
+    $("#f00px").slideDown('slow');
   });
   
   $("#findyourphotog_button").on('click', function(){
     $("#f00px").hide();
-    $("#findyourphotog").show();
+    $("#findyourphotog").hide();
+    $("#findyourphotog").slideDown('slow');
   }); 
 });
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -36,10 +36,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [280, 280]
   end
   
-  version :show do
-    process :resize_to_fill => [800, 800]
-  end
-  
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
