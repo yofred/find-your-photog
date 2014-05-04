77c53b43c446d3c57d63e785b86cb8b8ac9c1782
Yofred Moik <ytmoik@gmail.com>
2014-05-03 22:34:58 -0400

    logo with css animation

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
   $("#photographer-form").hide();
   $("#photographer-form-button").on('click', function(){
     $(this).hide();
-    $('#photographer-form').slideDown('slow').trigger('autosize.resize');
+    $('#photographer-form').slideDown('slow')
   });
   
   $("#user-form").hide();
   $("#user-form-button").on('click', function(){
     $(this).hide();
-    $('#user-form').slideDown('slow').trigger('autosize.resize');
+    $('#user-form').slideDown('slow')
   });
 });
 
