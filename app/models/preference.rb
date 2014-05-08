7451a835af1243a101857366c1ae46f1827d4377
Yofred Moik <ytmoik@gmail.com>
2014-05-08 11:49:58 -0400

    removed unused elements

--- app/models/preference.rb
+++
@@ -1,5 +0,0 @@
-class Preference < ActiveRecord::Base
-  attr_accessible :artistic, :conceptual, :contemporary, :documentary, :fine_art, :glamorous, :retro, :traditional, :user_id
-  
-  belongs_to :user
-end
--- app/models/style.rb
+++
@@ -1,3 +0,0 @@
-class Style < ActiveRecord::Base
-  attr_accessible :artistic, :conceptual, :contemporary, :documentary, :fine_art, :glamorous, :like, :photo_id, :photographer_id, :retro, :traditional, :user_id
-end
